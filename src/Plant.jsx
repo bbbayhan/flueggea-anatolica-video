674dc09210276ebb416593db7e01d4380d00b1b9
bbbayhan <bestebbayhan@gmail.com>
2021-06-24 10:26:26 +0200

    Add new icons to interfae

--- src/Plant.jsx
+++ src/Plant.jsx
@@ -1,30 +1,120 @@
-import React, {useState} from "react";
-import {IMAGE_SRC} from "./plantImages/MapNameToImage";
-import Youtube from 'react-youtube';
+import React, { useState } from "react";
+import { IMAGE_SRC } from "./plantImages/MapNameToImage";
+import Youtube from "react-youtube";
+import AddLocation from "@material-ui/icons/AddLocation";
+import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
+import ErrorIcon from "@material-ui/icons/Error";
 
-export const Plant = ({data}) => {
+const useStyles = makeStyles((theme) => ({
+  red: {
+    color: theme.palette.getContrastText(red[900]),
+    backgroundColor: red[900],
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+    fontSize: "0.4rem",
+    width: theme.spacing(2),
+    height: theme.spacing(2),
+  },
+}));
+
+export const Plant = ({ data }) => {
   const [open, setOpen] = useState(false);
+  const [color, setColor] = useState("#7A1DC9");
+  const buttonStyles = {
+    color: `${color}`,
+  };
+  const classes = useStyles();
   const opts = {
-    height: '100%',
-    width: '100%',
+    height: "100%",
+    width: "100%",
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
 
-
   return open ? (
-      <>
-       <Youtube videoId="kqUQn2igu6A" opts={opts}/>
-       </>
-    )
-      :
-      (
     <>
-      <img src={IMAGE_SRC[data.name]} width="140px"/>
-      <p style={{textAlign:"center", color:"#7A1DC9", fontStyle:"italic"}}>{data.name}</p>
-      <button onClick={()=>setOpen(true)} style={{textDecoration: 'none', color:'#0866C2', fontWeight:'bold', fontFamily:'Courier New', fontSize:'1rem'}}>Open the video</button>
-      </>
-      )
+      <Youtube videoId="kqUQn2igu6A" opts={opts} />
+    </>
+  ) : (
+    <>
+      <img src={IMAGE_SRC[data.name]} width="140px" alt="plant_image" />
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <span
+            style={{
+              fontFamily: "Avenir",
+              fontSize: "0.6rem",
+              color: "#7A1DC9",
+            }}
+          >
+            {data.city}
+          </span>
+          <AddLocation style={{ color: "#7A1DC9" }} />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <p
+            style={{
+              color: "#7A1DC9",
+              fontFamily: "Avenir",
+              fontSize: "0.6rem",
+            }}
+          >
+            {data.name}
+          </p>
+          <Avatar className={classes.red}>{data.iucnDegree}</Avatar>
+        </div>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <span
+            style={{
+              fontFamily: "Avenir",
+              fontSize: "0.6rem",
+              color: "#7A1DC9",
+            }}
+          >
+            {data.year}
+          </span>
+          <CalendarTodayIcon fontSize="small" style={{ color: "#7A1DC9" }} />
+        </div>
+        <div style={{ display: "flex", justifyContent: "space-between", marginTop:'0.5rem' }}>
+          <span
+            style={{
+              fontFamily: "Avenir",
+              fontSize: "0.6rem",
+              color: "#7A1DC9",
+            }}
+          >
+            {data.reason}
+          </span>
+          <ErrorIcon fontSize="small" style={{ color: "#7A1DC9" }} />
+        </div>
+
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <YouTubeIcon
+            onClick={() => setOpen(true)}
+            onMouseEnter={() => {
+              setColor("#c83f49");
+            }}
+            onMouseLeave={() => {
+              setColor("#7A1DC9");
+            }}
+            fontSize="large"
+            style={buttonStyles}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
--- src/plantImages/MapNameToImage.js
+++ src/plantImages/MapNameToImage.js
@@ -1,11 +1,22 @@
 import FlueggeaAnatolica from './FlueggeaAnatolica.jpeg';
-import CampanulaTroegerae from './CampanulaTroegerae.jpg';
-import GalanthusTrojanus from './GalanthusTrojanus.jpg';
-import LathyrusBelinensis from './LathyrusBelinensis.jpg';
+import LathyrusTrachycarpus from './LathyrusTrachycarpus.jpeg';
+import CentaureaRhizocalathium from './CentaureaRhizocalathium.jpeg';
+import GentianaLutea from './GentianaLutea2.png';
+import AnemoneBlanda from './AnemoneBlanda.jpeg';
+import CentaureasDavisii from './CentaureasDavisii.jpeg';
+import CampanulaAghrica from './CampanulaAghrica.jpg';
+import CentaureaTchihatcheffii from './CentaureaTchihatcheffii.jpeg';
+import LathyrusUndulatos from './LathyrusUndulatos.jpeg';
+
 
 export const IMAGE_SRC={
     "Flueggea Anatolica": FlueggeaAnatolica,
-    "Campanula Troegerae": CampanulaTroegerae,
-    "Galanthus Trojanus" :GalanthusTrojanus,
-    "Lathyrus Belinensis": LathyrusBelinensis
+    "Lathyrus Trachycarpus": LathyrusTrachycarpus,
+    "Centaurea Rhizocalathium" :CentaureaRhizocalathium,
+    "Gentiana Lutea": GentianaLutea,
+    "Anemone Blanda": AnemoneBlanda,
+    "Centaureas Davisii": CentaureasDavisii,
+    "Campanula Aghrica" :CampanulaAghrica,
+    "Centaurea Tchihatcheffii": CentaureaTchihatcheffii,
+    "Lathyrus Undulatos": LathyrusUndulatos,
 }
